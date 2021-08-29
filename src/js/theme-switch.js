ecaa5d707f612ae8d6f51cd7da286b579e3bba9f
Vladimir Bondar <vladi.bondar@gmail.com>
2021-08-29 19:18:54 +0300

    optimize one more time

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -7,7 +7,7 @@ setTheme((themeSwitch.checked = localStorage.getItem(PROP) === DARK));
 themeSwitch.onchange = e => setTheme(e.target.checked);
 
 function setTheme(isThemeSwitchChecked) {
-  document.body.classList.add(isThemeSwitchChecked ? DARK : LIGHT);
-  document.body.classList.remove(isThemeSwitchChecked ? LIGHT : DARK);
   localStorage.setItem(PROP, isThemeSwitchChecked ? DARK : LIGHT);
+  document.body.classList.add(localStorage[PROP]);
+  document.body.classList.remove(isThemeSwitchChecked ? LIGHT : DARK);
 }
